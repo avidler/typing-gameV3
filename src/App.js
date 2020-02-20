538fffaebd156216368cd270ff344d9b3ceae722
avidler <a.vidler@live.co.uk>
2020-02-20 10:13:41 +0000

    updated version 20/02

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
+import Navbar from "./components/navbar.component"
+import Game from "./components/game.component"
+import Scoreboard from "./components/scoreboard.component"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+  <Router>
+  <div className="container">
+    <Navbar />
+    <Route path="/" exact component={Game} />
+    <Route path="/scoreboard" exact component={Scoreboard} />
+  </div>
+</Router>
+  )
 }
 
 export default App;
